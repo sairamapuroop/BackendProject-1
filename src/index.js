aca9d6a60958956011bc2b52afd87d678f2b407a
Sai Ram Apuroop <apuroop21@gmail.com>
2024-04-23 05:33:46 +0530

    connect to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require('dotenv').config({path: './env'});
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await Mongoose.connect(`process.env.MONGODB_URI/${DB_NAME}`);
+
+    app.on("error", (err) => {
+      console.log("Error: " + err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`lostening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+})();
+*/
